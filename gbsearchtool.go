0dd23a68064ef96b2acabb9f4653fcec89ff4dd7
Guy Oliver <guy.oliver@gmail.com>
2013-08-19 09:16:04 -0400

    added options to search

--- gbsearchtool.go
+++ gbsearchtool.go
@@ -11,7 +11,10 @@ var isbn = flag.String("isbn", "0394758269", "ISBN number")
 
 func main() {
 	flag.Parse()
-	res, _ := gbsearch.ISBNSearch(*isbn)
+	opt := gbsearch.DefaultOptions()
+	opt.SetLanguageCode("en")
+	opt.SetPrintType(gbsearch.All)
+	res, _ := gbsearch.ISBNSearch(*isbn, opt)
 	out, _ := json.MarshalIndent(res, "", "  ")
 	log.Println(string(out))
 }
