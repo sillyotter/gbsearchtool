3f8e156959e9b55e075c516093bc0ed58af892f7
Guy Oliver <guy.oliver@gmail.com>
2013-08-19 20:06:48 -0400

    tweaks to command line handling

--- gbsearchtool.go
+++ gbsearchtool.go
@@ -2,78 +2,119 @@ package main
 
 import (
 	"encoding/json"
-	"errors"
 	"flag"
+	"fmt"
 	"github.com/sillyotter/gbsearch"
-	"log"
-	"os"
+	"strings"
 )
 
 var (
-	ErrUsageNotFound    = errors.New("Usage section not found")
-	ErrMoreThanOneUsage = errors.New("Usage section found more than once")
+	searchType   string
+	languageCode string
+	printType    string
+	filter       string
+	maxResults   int
+	startIndex   int
+	orderBy      string
+	projection   string
+	onlyEPub     bool
 )
 
-func parseSection(section string, doc string) ([]string, error) {
-	return make([]string, 0), nil
+func init() {
+	flag.StringVar(&searchType, "searchType", "unknown", "Set the type of search (title|author|publisher|subject|isbn|lccn|oclc)")
+	flag.StringVar(&languageCode, "languageCode", "", "Set the two character language code (en|fr|...)")
+	flag.StringVar(&printType, "mediaType", "", "Set the media type (all|book|magazine)")
+	flag.StringVar(&filter, "contentType", "", "(any|full|partial|free-ebooks|paid-ebooks|ebooks)")
+	flag.IntVar(&maxResults, "maxResults", -1, "Set maximum number of results to return <= 40")
+	flag.IntVar(&startIndex, "startIndex", -1, "Set starting offset for fetch")
+	flag.StringVar(&orderBy, "orderBy", "", "Sorty by (newest|relevance)")
+	flag.StringVar(&projection, "resultType", "", "(full|lite)")
+	flag.BoolVar(&onlyEPub, "onlyEPub", false, "Only search downloadable")
 }
 
-func parsePattern(usage string, options []string) error {
-	return nil
-}
-
-func parseDefaults(doc string) ([]string, error) {
-	return make([]string, 0), nil
-}
-
-func formalUsage(section string) string {
-	return ""
-}
-
-func docopt(doc string, argv []string, help bool, version string, optionsFirst bool) (map[string]string, error) {
-	args := argv[1:]
-	log.Println(args)
-
-	usage_sections, err := parseSection("usage:", doc)
-	if err != nil {
-		return nil, err
+func determinePrintType(val string) gbsearch.PrintType {
+	switch strings.ToLower(val) {
+	case "all":
+		return gbsearch.All
+	case "book":
+		return gbsearch.Books
+	case "magazine":
+		return gbsearch.Magazines
 	}
-
-	if len(usage_sections) == 0 {
-		return nil, ErrUsageNotFound
-	}
-
-	if len(usage_sections) > 1 {
-		return nil, ErrMoreThanOneUsage
-	}
-
-	options, err := parseDefaults(doc)
-	parsePattern(formalUsage(usage_sections[0]), options)
-
-	return make(map[string]string), nil
+	return gbsearch.UnknownPrintType
 }
 
-var isbn = flag.String("isbn", "0394758269", "ISBN number")
+func determineFilterType(val string) gbsearch.FilterType {
+	switch strings.ToLower(val) {
+	case "full":
+		return gbsearch.FullText
+	case "partial":
+		return gbsearch.PartialText
+	case "free-ebooks":
+		return gbsearch.FreeEbooks
+	case "paid-ebooks":
+		return gbsearch.PaidEbooks
+	case "ebooks":
+		return gbsearch.EBooks
+	}
+	return gbsearch.UnknownFilterType
+}
+
+func determineOrderBy(val string) gbsearch.OrderType {
+	switch strings.ToLower(val) {
+	case "relevance":
+		return gbsearch.Relevance
+	case "newest":
+		return gbsearch.Newest
+	}
+	return gbsearch.UnknownOrderByType
+}
+
+func determineProjection(val string) gbsearch.ProjectionType {
+	switch strings.ToLower(val) {
+	case "full":
+		return gbsearch.FullResults
+	case "lite":
+		return gbsearch.Lite
+	}
+	return gbsearch.UnknownProjectionType
+}
 
 func main() {
-	doc := `
-Usage:
-    my_program tcp <host> <port> [--timeout=<seconds>]
-    my_program serial <port> [--baud=<n>] [--timeout=<seconds>]
-    my_program (-h | --help | --version)
-
-Options:
-    -h, --help  Show this screen and exit.
-    --baud=<n>  Baudrate [default: 9600]`
-
-	x, _ := docopt(doc, os.Args, true, "", false)
-	log.Println(x["tcp"])
 
 	flag.Parse()
 	opt := gbsearch.DefaultOptions()
-	opt.SetLanguageCode("en")
-	opt.SetPrintType(gbsearch.All)
-	res, _ := gbsearch.ISBNSearch(*isbn, opt)
-	out, _ := json.MarshalIndent(res, "", "  ")
-	log.Println(string(out))
+	opt.SetLanguageCode(languageCode)
+	opt.OnlyFindEPubDownloads(onlyEPub)
+	opt.SetStartIndex(startIndex)
+	opt.SetMaxResults(maxResults)
+	opt.SetPrintType(determinePrintType(printType))
+	opt.SetFilter(determineFilterType(filter))
+	opt.SetOrderBy(determineOrderBy(orderBy))
+	opt.SetProjection(determineProjection(projection))
+
+	var st gbsearch.SearchType = gbsearch.UnknownSearchType
+
+	switch strings.ToLower(searchType) {
+	case "title":
+		st = gbsearch.InTitle
+	case "author":
+		st = gbsearch.InAuthor
+	case "publisher":
+		st = gbsearch.InPublisher
+	case "subject":
+		st = gbsearch.Subject
+	case "isbn":
+		st = gbsearch.ISBN
+	case "lccn":
+		st = gbsearch.LCCN
+	case "oclc":
+		st = gbsearch.OCLC
+	}
+
+	for _, elem := range flag.Args() {
+		res, _ := gbsearch.Search(st, elem, opt)
+		out, _ := json.MarshalIndent(res, "", "  ")
+		fmt.Println(string(out))
+	}
 }
